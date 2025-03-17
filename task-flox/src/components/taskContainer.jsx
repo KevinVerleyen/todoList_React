18c0b65bc795b2a95fc269f5a70e0053827e424b
KevinVerleyen <verleyen-kevin@hotmail.be>
2025-03-17 14:45:09 +0100

    input

--- task-flox/src/components/taskContainer.jsx
+++ task-flox/src/components/taskContainer.jsx
@@ -1,13 +1,37 @@
+import { useState } from "react";
 import { Footer } from "./footer/footer";
 import { Header } from "./header/header";
 import { TaskInput } from "./taskInput/taskInput";
 import { TaskList } from "./taskList/taskList";
 
 export const TaskContainer = () => {
+
+  const [taskList, setTaskList] = useState([
+    // {
+    //   id: 1,
+    //   title: "Faire la vaiselle",
+    //   completed: false,
+    // },
+    // {
+    //   id: 2,
+    //   title: "Sortir le chien",
+    //   completed: true,
+    // }
+  ]);
+
+  const addTask = (title) => {
+    const newTask = {
+      id: taskList.length + 1,
+      title: title,
+      completed: false,
+    };
+    setTaskList([...taskList, newTask])
+  }
+
   return (
     <main>
       <Header />
-      <TaskInput />
+      <TaskInput addTask={addTask}/>
       <TaskList />
       <Footer/>
     </main>
--- task-flox/src/components/taskInput/taskInput.jsx
+++ task-flox/src/components/taskInput/taskInput.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import styles from "./taskInput.module.css";
-export const TaskInput = () => {
+export const TaskInput = ({ addTask }) => {
+  const [taskTitle, setTaskTitle] = useState("");
+
+  const handleInputChange = (e) => {
+    setTaskTitle(e.target.value);
+  };
+
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    if (taskTitle.trim()) {
+      addTask(taskTitle);
+      setTaskTitle("");
+    }
+  };
+
   return (
     <div className={`box ${styles.element}`}>
       <h2 className={styles.title}>Ajoute ta prochaine tâche</h2>
-      <form className={styles.container}>
+      <form className={styles.container} onSubmit={handleAddTask}>
         <input
           type="text"
           className={styles.input}
           placeholder="Indequez un titre de tâche explicite."
+          onChange={handleInputChange}
+          value={taskTitle}
         />
         <button className="button-primary" type="submit">
           Ajouter
